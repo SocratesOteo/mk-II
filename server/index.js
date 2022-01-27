219c5e82d464d08a8d3d8d19b8aa66ab2f68949b
Paloma Oteo <oteokids@gmail.com>
2022-01-27 14:28:45 -0700

    5

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 require('dotenv').config()
 const path = require('path')
+const cors = require('cors')
 const port = process.env.PORT || process.env.SERVER_PORT
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.get('/',function(req,res) {
   res.sendFile(path.join(__dirname, '../login.html'));
