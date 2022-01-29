25ec5d3b4d26568d13ebe625f02306c17cecba3b
Paloma Oteo <oteokids@gmail.com>
2022-01-29 14:04:56 -0700

    8th

--- server/index.js
+++ server/index.js
@@ -16,17 +16,17 @@ require('dotenv').config()
 
 
 app.get('/',function(req,res) {
-  res.sendFile(path.join(__dirname, './login.html'));
+  res.sendFile(path.join(__dirname, '/login.html'));
 });
 app.get('/styles',(req, res) => {
-    res.sendFile(path.join(__dirname,'./login.css'))
+    res.sendFile(path.join(__dirname,'/login.css'))
 })
 
 app.get('/js',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../seed.js'))
+    res.sendFile(path.join(__dirname,'./server/seed.js'))
 })
 app.get('/js',(req,res)=>{
-    res.sendFile(path.join(__dirname,'./login.js'))
+    res.sendFile(path.join(__dirname,'/login.js'))
 })
 
 //app.post('/seed', seed)
