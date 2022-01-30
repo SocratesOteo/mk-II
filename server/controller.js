7e94b61707a63ddc4b564ef3f693b27aca6474e5
Paloma Oteo <oteokids@gmail.com>
2022-01-29 22:18:34 -0700

    33

--- server/controller.js
+++ server/controller.js
@@ -46,7 +46,7 @@ app.post('/user',(req,res)=>{
     const{username,email,password}= req.body
     sequelize.query(`
         INSERT INTO users(username,password,email)
-        VALUES(${username}, ${email},${password});
+        VALUES("${username}"," ${password}","${email}");
     
     `).then(dbRes => res.status(200).send(dbRes[0]))
 
