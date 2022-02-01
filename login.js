5aa41d49f3c70de1701f3a7353d4a364a3a62d83
Paloma Oteo <oteokids@gmail.com>
2022-02-01 15:00:20 -0700

    13

--- login.js
+++ login.js
@@ -20,8 +20,11 @@ function handleRegisterSubmit(e){
         axios.post('http://localhost:5777/user' || 'https://tec2-44-199-49-128.compute-1.amazonaws.com/user',body)
         .then(()=>{
             console.log('info sent over')
-            window.location.assign('/home')
+            window.location.assign('')
         })
+
+        axios.get()
+
     } else if (newPassword.value != confirmPassword.value){
         
         alert('Passwords Must Match')
