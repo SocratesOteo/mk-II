dfce1345e02ce61a59f2eb65fec5895a2d5f6eae
Paloma Oteo <oteokids@gmail.com>
2022-02-01 15:16:09 -0700

    199

--- login.js
+++ login.js
@@ -17,7 +17,7 @@ function handleRegisterSubmit(e){
         }
         console.log(body)
     
-        axios.post('http://localhost:5777/user' || 'https://tec2-44-199-49-128.compute-1.amazonaws.com/user',body)
+        axios.post('http://localhost:5777/user' || 'https://treasure-hunt-testing.herokuapp.com/user',body)
         .then(()=>{
             console.log('info sent over')
             window.location.href = 'home.html'
