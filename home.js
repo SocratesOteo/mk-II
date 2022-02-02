faff7e44bea37ca731e0b0bef365259a00ca0511
Paloma Oteo <oteokids@gmail.com>
2022-02-01 21:54:48 -0700

    3456

--- home.js
+++ home.js
@@ -5,5 +5,7 @@ const puzzlesButton = document.querySelector('#puzzles-button')
 const baseURL = 'https://treasure-hunt-testing.herokuapp.com' || 'http://localhost:5777'
 
 function clickLeaderBoard(){
-    window.location.href = `${baseURL}/leaderboard,`
+    window.location.href = `${baseURL}/leaderboard`
 }
+
+leaderboardButton.addEventListener('click', clickLeaderBoard)
